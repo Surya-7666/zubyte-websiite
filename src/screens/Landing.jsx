ffd65ce849e6c0c094cadc36b4cfdd3eeb18cf47
Surya <150419873+Surya-7666@users.noreply.github.com>
2025-07-21 14:17:09 +0530

    Update Landing.jsx

--- src/screens/Landing.jsx
+++ src/screens/Landing.jsx
@@ -2,10 +2,10 @@ import React from "react";
 // Sections
 import TopNavbar from "../components/Nav/TopNavbar";
 import Header from "../components/Sections/Header";
+import AboutUs from "../components/Sections/AboutUs";
 import Services from "../components/Sections/Services";
 import Projects from "../components/Sections/Projects";
 import Blog from "../components/Sections/Blog";
-import AboutUs from "../components/Sections/AboutUs";
 import Contact from "../components/Sections/Contact";
 import Footer from "../components/Sections/Footer"
 
