8a69ac852c5ebdc80dc5c1f5e9a0e5eaae9b5da1
Surya <150419873+Surya-7666@users.noreply.github.com>
2025-07-25 16:52:27 +0530

    Add files via upload

---
+++ src/assets/svg/MobileLogo.jsx
@@ -0,0 +1,48 @@
+import * as React from "react";
+
+function SvgComponent1(props) {
+  return (
+   <svg width="52" height="37" viewBox="0 0 77 77" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g filter="url(#filter0_dd_187_2)">
+<rect x="4" y="4" width="65" height="65" rx="10" fill="white"/>
+<g filter="url(#filter1_d_187_2)">
+<path d="M12 17.2668H51.0058C55.3142 17.2668 58.8069 20.7595 58.8069 25.068C58.8069 29.3764 55.3142 32.8691 51.0058 32.8691H27.6023C18.9854 32.8691 12 25.8837 12 17.2668Z" fill="#233232"/>
+<path d="M46.9549 45.0175C49.0261 41.6223 53.4574 40.5491 56.8526 42.6203C60.2477 44.6915 61.3209 49.1228 59.2497 52.518C57.1785 55.9131 52.7472 56.9864 49.352 54.9151C45.9569 52.8439 44.8837 48.4126 46.9549 45.0175Z" fill="#233232"/>
+<path d="M45.0447 18.9657C48.5991 15.9833 53.8982 16.4469 56.8807 20.0012C59.8631 23.5556 59.3995 28.8547 55.8452 31.8372L30.7154 52.9235C27.1611 55.906 21.862 55.4423 18.8795 51.888C15.897 48.3336 16.3607 43.0345 19.915 40.0521L45.0447 18.9657Z" fill="#C6FF53"/>
+</g>
+</g>
+<defs>
+<filter id="filter0_dd_187_2" x="0" y="0" width="77" height="77" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+<feFlood flood-opacity="0" result="BackgroundImageFix"/>
+<feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+<feOffset dy="4"/>
+<feGaussianBlur stdDeviation="2"/>
+<feComposite in2="hardAlpha" operator="out"/>
+<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_187_2"/>
+<feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+<feOffset dx="4"/>
+<feGaussianBlur stdDeviation="2"/>
+<feComposite in2="hardAlpha" operator="out"/>
+<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+<feBlend mode="normal" in2="effect1_dropShadow_187_2" result="effect2_dropShadow_187_2"/>
+<feBlend mode="normal" in="SourceGraphic" in2="effect2_dropShadow_187_2" result="shape"/>
+</filter>
+<filter id="filter1_d_187_2" x="10" y="17" width="52.3042" height="42.9707" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+<feFlood flood-opacity="0" result="BackgroundImageFix"/>
+<feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+<feOffset dy="2"/>
+<feGaussianBlur stdDeviation="1"/>
+<feComposite in2="hardAlpha" operator="out"/>
+<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_187_2"/>
+<feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_187_2" result="shape"/>
+</filter>
+</defs>
+</svg>
+
+
+  );
+}
+
+export default SvgComponent1;
